2a827ff904dd8af3204fe3be94d0ae314d948032
Enrique <enrique.navarro@ingenieros.com>
2022-11-04 18:30:01 +0100

    Initial commit

---
+++ src/main/java/com/university/app/AppUniversityApplication.java
@@ -0,0 +1,13 @@
+package com.university.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppUniversityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppUniversityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/university/app/AppUniversityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.university.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppUniversityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
