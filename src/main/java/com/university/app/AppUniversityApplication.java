59a7f93e30f50e16781d4ebfeba50f8665390346
Enrique <enrique.navarro@ingenieros.com>
2022-11-10 23:26:51 +0100

    Add context path

--- src/main/java/com/university/app/AppUniversityApplication.java
+++ src/main/java/com/university/app/AppUniversityApplication.java
@@ -1,14 +1,7 @@
 package com.university.app;
 
-import com.university.app.entity.Alumno;
-import com.university.app.entity.Direccion;
-import com.university.app.entity.Persona;
-import com.university.app.servicios.AlumnoService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AppUniversityApplication {
